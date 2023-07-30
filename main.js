6aee1456e7c340ec967403cabddf91882be2c212
Keana Cobarde <keanacobarde@gmail.com>
2023-07-30 12:23:39 -0500

    creating HTML structure and draft of student arrays for both students and expelledStudents

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
+// *********  DATA ********** //
+//STUDENTS ARRAY // 
+const students = [ 
+  { 
+  studentId: 1,
+  name: "Harry Potter", 
+  house: "Gryffindor", 
+  },
+  { 
+  studentId: 2,
+  name: "Luna Lovegood", 
+  house: "Ravenclaw", 
+  }, 
+  { 
+  studentId: 3,
+  name: "Draco Malfoy", 
+  house: "Slythirin", 
+  },
+  { 
+  studentId: 4,
+  name: "Cedric Diggory", 
+  house: "Hufflepuff", 
+  },
+  { 
+  studentId: 5,
+  name: "Cho Chang", 
+  house: "Ravenclaw", 
+  },    
+  ]
 
+  //Exeplled Students Array
+const expelledStudents = []; 
+// *********  UTILITY FUNCTIONS  ********* //
