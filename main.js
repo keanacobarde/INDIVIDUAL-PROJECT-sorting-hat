1da01222703e3ae8120ecf69723049482a035532
Keana Cobarde <keanacobarde@gmail.com>
2023-07-30 19:43:26 -0500

    created expulsion functionality, more styling is needed

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ renderToDom("#formid", form);
 
 const filterBtns = `<button type="button" class="btn btn-outline-dark" id="all">All</button><button type="button" class="btn btn-outline-danger" id="gryf">Gryffindor</button><button type="button" class="btn btn-outline-success" id="sly">Slytherin</button><button type="button" class="btn btn-outline-primary"id="rave">Ravenclaw</button><button type="button" class="btn btn-outline-info"id="huff">Hufflepuff</button>`
 
-//Rendering Student Cards
+//Rendering Student Cards - Students
 const cardsOnDom = (array) => {
  let domString = ``; 
 array.forEach(student => {
@@ -67,6 +67,28 @@ domString += `<div class="card mb-3" style="max-width: 500px;">
 renderToDom("#studentsid", domString); 
 } 
 
+//Rendering Cards - For Expelled Students
+const cardsOnDomExpelled = (array) => {
+  let domString = ``; 
+ array.forEach(student => {
+ domString += `<div class="card mb-3" style="max-width: 500px;">
+ <div class="row g-0">
+   <div class="col-md-4"style="max-width:150px">
+     <img style="height:150px;width:150px;"src="https://hexcolorpedia.com/wp-content/themes/bootscore-5-child/solid.php?c=80080a" alt="...">
+   </div>
+   <div class="col-md-8">
+     <div class="card-body">
+       <h5 class="card-title">${student.name}</h5>
+       <p class="card-text">${student.house}</p>
+       <p class="card-text"><small class="text-body-secondary"><button type="button" class="btn btn-outline-danger" id='expel--${student.studentId}'>Expelled!</button></small></p>
+     </div>
+   </div>
+ </div>
+ </div>`
+ });
+ renderToDom("#expelledid", domString); 
+ } 
+
 // *********  EVENT LISTENERS  *********  //
 const eventListeners = () => {
 //FILTER BUTTONS 
@@ -86,10 +108,13 @@ document.querySelector("#filterbtnid").addEventListener('click', (e) => {
 })
 
 //Buttons on Cards - Expel 
-document.querySelector("#studentsid").addEventListener('click', (e) => {
+document.querySelector(".array_container").addEventListener('click', (e) => {
   if (e.target.id.includes('expel')) {
   const [ , studentToDel] = e.target.id.split('--'); 
-  console.log(studentToDel);
+  expelledStudents.push(students[parseInt(studentToDel) - 1]);
+  students.splice(parseInt(studentToDel) - 1, 1);
+  cardsOnDom(students);
+  cardsOnDomExpelled(expelledStudents);    
   }
 }); 
 
